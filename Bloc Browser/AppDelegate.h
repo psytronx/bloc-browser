4730f01fbb6c25cf9cfcefc0ebe53ea3889dd975
Roger Hom <roger.c.hom@gmail.com>
2015-07-17 13:24:15 -0700

    Add initial files

---
+++ Bloc Browser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bloc Browser
+//
+//  Created by psytronx on 7/17/15.
+//  Copyright (c) 2015 Logical Dimension. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bloc Browser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bloc Browser
+//
+//  Created by psytronx on 7/17/15.
+//  Copyright (c) 2015 Logical Dimension. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
